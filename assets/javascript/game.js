7f919a38a3ee4111e42d4bcf6e30b7f2d03a8c2c
Jrew <jrewmohamed@gmail.com>
2018-09-09 21:24:44 -0400

    Fixed win/loss counters, restart function, and onclick functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,31 +8,92 @@ $("#computerGuess").text(computerRandomNumber);
 var wins = 0;
 var losses = 0;
 
-//Adds the win and loss results to the DOM
-$("#results").append(`<h4>wins = ${wins}</h4>`);
-$("#results").append(`<h4>losses = ${losses}</h4>`);
+//Declares the state of the game
+var winner = false;
+var lossUpdated = false;
 
-var userRandomNumber = (Math.floor(Math.random() * 13) + 1);
+//Assigns a random number to each image
+var userRandomEmerald = (Math.floor(Math.random() * 13) + 1);
+var userRandomRuby = (Math.floor(Math.random() * 13) + 1);
+var userRandomSapphire = (Math.floor(Math.random() * 13) + 1);
+var userRandomAmethyst = (Math.floor(Math.random() * 13) + 1);
 
 // Declares the userScore variable to the DOM
 var userScore = 0;
 $("#userGuess").text(userScore);
 
-//Shows the sum of all of the users guesses in the DOM
-$("#emerald").click(function() {
-    $("#userGuess").text(userScore = userScore + userRandomNumber);
-})
+//Creates a reset function
+function reset() {
+    computerRandomNumber = Math.floor((Math.random() * 120) + 19);
+    $("#computerGuess").text(computerRandomNumber);
+    userScore = 0;
+    $("#userGuess").text(userScore);
+    userRandomEmerald = (Math.floor(Math.random() * 13) + 1);
+    userRandomRuby = (Math.floor(Math.random() * 13) + 1);
+    userRandomSapphire = (Math.floor(Math.random() * 13) + 1);
+    userRandomAmethyst = (Math.floor(Math.random() * 13) + 1);
+    $("#message").html("");
+    winner = false;
+}
 
-$("#ruby").click(function() {
-    $("#userGuess").text(userScore = userScore + userRandomNumber);
-})
+//Creates an onclick function to call the reset function
+function restart() {
+    $("#message").click(function () {
+        reset();
+    })
+};
 
-$("#sapphire").click(function() {
-    $("#userGuess").text(userScore = userScore + userRandomNumber);
-})
+//Checks to see if userGuess is equal to/over computerRandomNumber 
+function winChecker() {
+    if (userScore === computerRandomNumber) {
+        wins++;
+        $("#win").text(wins);
+        $("#message").html('<h4 id="winMessage">Congrats you win! Click Me to play again!</h4>');
+        restart();
+        winner = true;
+    } else if ((userScore > computerRandomNumber) && (winner === false) && (lossUpdated === false)) {
+        losses++;
+        $("#loss").text(losses);
+        $("#message").html('<h4 id="lossMessage">Sorry you lost! Click Me to play again!</h4>');
+        restart();
+        lossUpdated = true;
+    };
+};
 
-$("#amethyst").click(function() {
-    $("#userGuess").text(userScore = userScore + userRandomNumber);
-})
+$(document).ready(function () {
+
+    //Shows the sum of all of the users guesses in the DOM
+    $("#emerald").click(function () {
+        userScore = userScore + userRandomEmerald;
+        $("#userGuess").text(userScore);
+        winChecker();
+        console.log(userRandomEmerald);
+    })
+
+    $("#ruby").click(function () {
+        userScore = userScore + userRandomRuby;
+        $("#userGuess").text(userScore);
+        winChecker();
+        console.log(userRandomRuby);
+    })
+
+    $("#sapphire").click(function () {
+        userScore = userScore + userRandomSapphire;
+        $("#userGuess").text(userScore);
+        winChecker();
+        console.log(userRandomSapphire);
+    })
+
+    $("#amethyst").click(function () {
+        userScore = userScore + userRandomAmethyst;
+        $("#userGuess").text(userScore);
+        winChecker();
+        console.log(userRandomAmethyst);
+    })
+
+});
+
+
+
 
 
--- pseudocode.md
+++ pseudocode.md
@@ -1,27 +1,37 @@
-make title;
+*make title;
 
-make instructions;
+*make instructions;
 
-make userGuess counter;
+*make computerRandomNumber;
+  *umber should be from 19 - 120;
 
-make computerRandomNumber;
-  number should be from 19 - 120;
+*make userGuess counter;
 
 make win counter;
   if userGuess is equal to computerRandomNumber win;
-    win counter goes up by 1;
-    print winStatement;
+    *win counter goes up by 1;
+    *print winStatement;
     reset computerRandomNumber;
     reset gemRandomNumber for each gem;
 
 make loss counter;
   if userGuess > computerRandomNumber;
-    loss counter goes up by 1;
-    print lossStatement;
+    *loss counter goes up ONLY 1;
+    *print lossStatement;
     reset computerRandomNumber;
+    reset gemRandomNumber for each gem;
 
-make 4 different gem pictures;
-  have each gem hold a different random number:
-    random number should be from 1 - 12;
-  if user clicks on a gem:
-    userGuess counter goes up by the random number that was on the gem that they clicked;
+*make 4 different gem pictures (Ruby, Sapphire, Emerald, Topaz);
+  *have each gem hold a different random number:
+    *random number should be from 1 - 12;
+  *if user clicks on a gem:
+    *userGuess counter goes up by the random number that was on the gem that they clicked;
+
+TODO:
+
+Add symmetrical height to the results and message id;
+Set onclick function to reset the game;
+Set reset computerRandomNumber;
+Set reset gemRandomNumber for each gem;
+
+
